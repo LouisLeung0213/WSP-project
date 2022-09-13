c915317f981765fcd272903b3c38c37bcc197261
Louis Leung <louizleung@gmail.com>
2022-09-13 20:14:13 +0800

    update signUp alert and some checking of signup form

--- public/signUp/signUp.js
+++ public/signUp/signUp.js
@@ -26,18 +26,53 @@ signUpForm.addEventListener("click", async (event) => {
   // console.log(birthday.value);
   // console.log(formData);
   // console.log(image.files);
-  await fetch("/signUp", {
+  // await fetch("/signUp", {
+  //   method: "POST",
+  //   body: formData,
+  // })
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //     window.location = "/lobby/lobby.html";
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  const res = await fetch("/signUp", {
     method: "POST",
     body: formData,
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-      window.location = "/lobby/lobby.html";
-    })
-    .catch((err) => {
-      console.log(err);
+  });
+  let json = await res.json();
+  console.log(json);
+  if (!res.ok) {
+    if (res.status == 400) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Username already been used, please use another username",
+      });
+    } else if (res.status == 403) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Email already been used, please use another email",
+      });
+    } else if (res.status == 406) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Nickname already been used, please use other name",
+      });
+    }
+  } else {
+    Swal.fire({
+      icon: "success",
+      title: `Welcome, ${nickName.value}`,
+      showConfirmButton: true,
+    }).then((result) => {
+      if (result.isConfirmed) window.location = "/lobby/lobby.html";
     });
+  }
 });
